08d6d2e0805e337aa1eb59ca7921f886973a1c02
Caleb Cargill <cargilch@mail.uc.edu>
2024-11-14 23:43:07 -0500

    remove emailjs references

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,24 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const emailjs = require('@emailjs/browser');
 
 router.post('/contact', async (req, res) => {
     const { sender, message, email, subject } = req.body;
-    const serviceId = process.env.EMAILJS_SERVICE_ID;
-    const templateId = process.env.EMAILJS_TEMPLATE_ID;
-    const key = process.env.EMAILJS_PUBLIC_KEY;
 
     try {
-        console.log("sending message");
-        emailjs.send(serviceId, templateId, {
-            name: sender,
-            message: message,
-            reply_to: email,
-            subject: subject,
-        }, {
-            publicKey: key,
-            blockHeadless: true,
-        });
+
         console.log("message sent");
     } catch (err) {
         console.error(err.message);
