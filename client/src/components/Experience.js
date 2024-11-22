62c19112ee6267d84b0959399692216a46ba2b88
Caleb Cargill <cargilch@mail.uc.edu>
2024-11-21 22:19:11 -0500

    creation of experiences radio buttons and initial styling

--- client/src/components/Experience.js
+++ client/src/components/Experience.js
@@ -4,38 +4,35 @@ import axios from 'axios';
 
 const Experience = () => {
     const [experiences, setExperiences] = useState([]);
+    const [selectedCompany, setSelectedCompany] = useState('');
 
     useEffect(() => {
         axios.get('http://localhost:5000/api/experiences')
         .then(response => setExperiences(response.data))
+        .then(() => setSelectedCompany(experiences.at(1).company))
         .catch(error => console.error(error));
-    }, []);
+    }, []);    
 
     return (
         <div>
             <h2 className="accentHeader">Experience</h2>
             <div className="gallery">
                 <div className="block">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Company</th>
-                                <th>Title</th>
-                                <th>Start</th>
-                                <th>End</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {experiences.map(experience => (
-                                <tr key={experience._id}>
-                                    <td>{experience.company}</td>
-                                    <td>{experience.title}</td>
-                                    <td>{new Date(experience.start).toISOString().split('T')[0]}</td>
-                                    <td>{new Date(experience.end).toISOString().split('T')[0]}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                    <div className="radio-group">
+                        {experiences.map(exp => (
+                            <label className="radio-label">
+                                <input name={exp.company} type='radio' id={exp.company} checked={selectedCompany === exp.company} onChange={() => setSelectedCompany(exp.company)} />
+                                <span className='inner-label'>{exp.company}</span>
+                            </label>
+                        ))}
+                    </div>
+                </div>
+                <div className="block">
+                    <ul>
+                        {/* {experiences.find(e => e.company === selectedCompany).descriptions.map(d => (
+                            <li>{d}</li>
+                        ))} */}
+                    </ul>
                 </div>
             </div>
         </div>
