f8b60577fe6a065692470c2f6b75eb87a3ccfb5a
caleb-cargill <cargilch@mail.uc.edu>
2025-06-18 20:40:11 -0400

    remove duplicated route

--- server/routes/info.js
+++ server/routes/info.js
@@ -13,14 +13,4 @@ router.get('/experiences', async (req, res) => {
     }
 });
 
-router.get('/projects', async (req, res) => {
-    try {
-        const projects = await Project.find();
-        res.json(projects);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error fetching projects', error });
-    }
-});
-
 module.exports = router;
